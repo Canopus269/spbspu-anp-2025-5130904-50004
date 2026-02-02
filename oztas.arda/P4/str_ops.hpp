08f459f37c3eb2032febadfa3eb9eb4042965e6d
Arda Boran Öztaş <oztas269@gmail.com>
2026-02-02 23:47:03 +0300

    Update str_ops.hpp

--- oztas.arda/P4/str_ops.hpp
+++ oztas.arda/P4/str_ops.hpp
@@ -3,24 +3,25 @@
 
 #include <cstddef>
 
-namespace oztas {
+namespace oztas
+{
 
-  size_t cstrLen(const char* s) noexcept;
+size_t cstrLen(const char* s) noexcept;
 
-  size_t replaceSymbol(
-    const char* src,
-    int fromCh,
-    int toCh,
-    char* dst,
-    size_t dstSize
-  ) noexcept;
+size_t replaceSymbol(
+  const char* src,
+  int fromCh,
+  int toCh,
+  char* dst,
+  size_t dstSize
+) noexcept;
 
-  size_t latinTwo(
-    const char* s1,
-    const char* s2,
-    char* dst,
-    size_t dstSize
-  ) noexcept;
+size_t latinTwo(
+  const char* s1,
+  const char* s2,
+  char* dst,
+  size_t dstSize
+) noexcept;
 
 }
 
