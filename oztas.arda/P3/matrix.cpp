efeba32d1f7245ba3d49f73e1caf12485dcf8f59
Arda Boran Öztaş <oztas269@gmail.com>
2026-02-04 11:54:02 +0300

    Update matrix.cpp

--- oztas.arda/P3/matrix.cpp
+++ oztas.arda/P3/matrix.cpp
@@ -4,13 +4,17 @@
 
 bool oztas::readMatrix(std::istream& input, int matrix[], size_t rows, size_t cols)
 {
-  if (rows == 0 || cols == 0) {
+  if (rows == 0 || cols == 0)
+  {
     return true;
   }
 
-  for (size_t i = 0; i < rows; ++i) {
-    for (size_t j = 0; j < cols; ++j) {
-      if (!(input >> matrix[i * cols + j])) {
+  for (size_t i = 0; i < rows; ++i)
+  {
+    for (size_t j = 0; j < cols; ++j)
+    {
+      if (!(input >> matrix[i * cols + j]))
+      {
         return false;
       }
     }
@@ -23,8 +27,10 @@ void oztas::writeMatrix(std::ostream& output, const int matrix[], size_t rows, s
 {
   output << rows << " " << cols;
 
-  for (size_t i = 0; i < rows; ++i) {
-    for (size_t j = 0; j < cols; ++j) {
+  for (size_t i = 0; i < rows; ++i)
+  {
+    for (size_t j = 0; j < cols; ++j)
+    {
       output << " " << matrix[i * cols + j];
     }
   }
